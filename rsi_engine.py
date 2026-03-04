083dcf7130ccdd5f70939fc688014545abcf6e78
Vishal170588 <124762612+Vishal170588@users.noreply.github.com>
2026-03-04 22:08:45 +0530

    Update rsi_engine.py

--- rsi_engine.py
+++ rsi_engine.py
@@ -202,9 +202,16 @@ def check_signal(symbol):
 def run():
 
     print("RSI INDEX ENGINE STARTED")
-
+    bot_alive()
+    
     while True:
-        bot_alive()
+        now = datetime.now()
+
+    market_open = now.replace(hour=9, minute=15, second=0, microsecond=0)
+    market_close = now.replace(hour=15, minute=30, second=0, microsecond=0)
+
+    if market_open <= now <= market_close:
+        
 
         for index in INDICES:
 
