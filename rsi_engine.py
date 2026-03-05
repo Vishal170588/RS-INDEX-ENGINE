ace426d4c55e19f62c0632b47522067c09e101a9
Vishal170588 <124762612+Vishal170588@users.noreply.github.com>
2026-03-05 12:37:45 +0530

    Update rsi_engine.py

--- rsi_engine.py
+++ rsi_engine.py
@@ -281,7 +281,7 @@ RSI: {round(rsi_now,2)}
 
 
 # Stage 2 trade expansion
-if not state.expansion_sent and rsi_now >= state.rsi_signal + 4:
+    if not state.expansion_sent and rsi_now >= state.rsi_signal + 4:
     send_telegram(f"""
 🚀 MOMENTUM EXPANSION
 
